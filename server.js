d0a0a70567696d8266f95595ac572668ae3d7891
Troy Wills <troy_wills@outlook.com>
2021-03-13 17:04:04 -0600

    heroku test

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 // Dependencies 
 const express = require("express");
+
 const app = express();
-const port = 3000; 
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(__dirname + '/public'));
+app.use(express.static("public"));
 
 // require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
-app.listen(port, () => {
-    console.log(`Our server is up and running on port: ${port}`);
+app.listen(PORT, () => {
+    console.log(`Our server is up and running on port: ${PORT}`);
 });
